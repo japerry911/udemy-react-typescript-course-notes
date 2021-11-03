d15a7a44b7d9bcf2d883f4ec7c8263648491f0cc
Jack Perry <jack.e.perry@outlook.com>
2021-11-03 07:22:32 -0600

    👌 IMPROVE: progress

---
+++ jbook/src/index.tsx
@@ -0,0 +1,39 @@
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = () => {
+    if (!ref.current) {
+      return;
+    }
+
+    console.log(ref.current);
+  };
+
+  return (
+    <div>
+      <textarea onChange={(e) => setInput(e.target.value)} value={input} />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
